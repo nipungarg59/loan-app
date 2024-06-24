c69e2a07192776722bce6cd74ed8eecdf4f0b350
Nipun Garg <nipungarg59@gmail.com>
2024-06-25 03:35:19 +0530

    Adds base models to users

--- users/models.py
+++ users/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 
-
+from generics.models.base_model import BaseModel
 from users.constants import USER_ROLE_CHOICES
 
 
 # Create your models here.
 
-class User(models.Model):
+class User(BaseModel):
     user_name = models.CharField(max_length=100, null=False, blank=False)
     password = models.CharField(max_length=128)
     role = models.CharField(max_length=100, choices=USER_ROLE_CHOICES, null=False, blank=False)
